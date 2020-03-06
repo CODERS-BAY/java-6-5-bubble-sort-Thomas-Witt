ca75d903ffa8d7f270fcfe4dcdb16bf616b2e58f
Thomas Witt <tom.89@gmx.at>
2020-03-06 10:48:43 +0100

    Hier wird simple und einfach gebubbled! - Jetzt NEU! Mit Methoden! 50% Rabatt!!!

--- src/bubblebath.java
+++ src/bubblebath.java
@@ -4,22 +4,14 @@ import java.util.Scanner;
 public class bubblebath {
     public static void main(String[] args) {
         int[] num = new int[0];
-        int input;
-        System.out.println("Bitte gib die zu sortierenden Zahlen ein:");
         Scanner scanner = new Scanner(System.in);
-
-        while (!scanner.hasNext("q") && !scanner.hasNext("Q")){
-
-            input = scanner.nextInt();
-            int[] temp = new int[num.length + 1];
-            for (int i = 0; i < num.length; i++) {
-                temp[i] = num[i];
-            }
-            temp[num.length] = input;
-            num = temp;
-        }
+        num = giveMeAllTheNumbers(num, scanner);
         System.out.println("Deine Zahlen sind:     " + Arrays.toString(num));
+        bubbelySortyThingy(num);
+        System.out.println("Die sortierten Zahlen: " + Arrays.toString(num));
+    }
 
+    private static void bubbelySortyThingy(int[] num) {
         for(int i=1; i<num.length; i++) {
             for (int n = 0; n < (num.length - i); n++){
                 if(num[n+1]<num[n]){
@@ -28,8 +20,21 @@ public class bubblebath {
                     num[n+1] = temp;
                 }
             }
-
         }
-        System.out.println("Die sortierten Zahlen: " + Arrays.toString(num));
+    }
+
+    private static int[] giveMeAllTheNumbers(int[] num, Scanner scanner) {
+        System.out.println("Bitte gib die zu sortierenden Zahlen ein:");
+        int input;
+        while (!scanner.hasNext("q") && !scanner.hasNext("Q")){
+            input = scanner.nextInt();
+            int[] temp = new int[num.length + 1];
+            for (int i = 0; i < num.length; i++) {
+                temp[i] = num[i];
+            }
+            temp[num.length] = input;
+            num = temp;
+        }
+        return num;
     }
 }
